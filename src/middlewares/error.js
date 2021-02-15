ae12d6a24c7e912d28b0de3b81b26cd859a1bfee
faparipik <bruncevicfaruk@gmail.com>
2021-02-15 23:53:43 +0100

    test

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -2,7 +2,7 @@ import status from 'http-status';
 
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
-  const { NODE_ENV = "development" } = process.env;
+  const { NODE_ENV = 'development' } = process.env;
   let { statusCode, message } = err;
   if (NODE_ENV === 'production') {
     statusCode = status.INTERNAL_SERVER_ERROR;
@@ -14,7 +14,7 @@ const errorHandler = (err, req, res, next) => {
   const response = {
     code: statusCode,
     message,
-    ...(NODE_ENV === 'development' && { stack: err.stack }),
+    ...(NODE_ENV === "development" && { stack: err.stack }),
   };
 
   if (NODE_ENV === 'development') {
